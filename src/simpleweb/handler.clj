ec250007ae5fefd1e98263a9b0056ccfdb1cfc91
김형기 <sokoban@gimhyeong-giui-MacBook-Pro.local>
2018-06-03 16:29:34 +0900

    first commit

---
+++ src/simpleweb/handler.clj
@@ -0,0 +1,31 @@
+(ns simpleweb.handler
+  (:require [simpleweb.views :as views]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(defroutes app-routes ; replace the generated app-routes with this
+  (GET "/"
+       []
+       (views/home-page))
+  (GET "/add-location"
+       []
+       (views/add-location-page))
+  (POST "/add-location"
+        {params :params}
+        (views/add-location-results-page params))
+  (GET "/location/:loc-id"
+       [loc-id]
+       (views/location-page loc-id))
+  (GET "/all-locations"
+       []
+       (views/all-locations-page))
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+;(defroutes app-routes
+;  (GET "/" [] "Hello World")
+;  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
